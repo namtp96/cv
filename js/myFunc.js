7f3f4d0bfaa79bfae8ed55ded126d99e4cd5eda8
namtp <iamcocodi@gmail.com>
2019-06-13 19:20:42 +0700

    thurday19h

--- js/myFunc.js
+++ js/myFunc.js
@@ -13,4 +13,42 @@ $(document).ready(function () {
             item.css('background-color', colors[Math.floor(Math.random() * colors.length)]);
         }, 100);
     }
-})
+    if($('#box1')){
+        $('#box1').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 1000
+        });
+    }
+    if($('#box1')){
+        $('#box2').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 1000
+        });
+    }
+    if($('#box3')){
+        $('#box3').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 1000
+        });
+    }
+});
+$('#button').click(function(){
+    $(this).toggleClass('clicked');
+    $('#menu').toggleClass('active');
+    $('body').toggleClass('overlay');
+});
